704a1790e3fddc890ca1ea0b32d7e1afc2d2b858
PedrolliMatteo <Pedro@HP.com>
2025-09-27 17:24:18 +0200

    Fatta funzione per aggiungere contatti e per visualizzre contatti

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 //ricorda di mettere prima le struct e poi il main
 
@@ -11,7 +12,51 @@ struct Contatto
 
 int main(void) {
 
-    struct Contatto contatti[20];
+    struct Contatto contatti[100];
+    int scelta, i = 0, controllo = 0;
+
+    while (controllo == 0)
+    {
+        printf("Cosa fare oggi nella rubrica?\n"
+           "1. Aggiungere contatto\n"
+           "2. Visualizzare tutti i contatti\n"
+           "3. Cercare un contatto per cognome\n"
+           "4. Eliminare un contatto per cognome\n"
+           "5. Uscire\n");
+
+        scanf("%d", &scelta);
+
+        switch (scelta)
+        {
+            case 1:
+                printf("Inserisci il nome del nuovo contatto:\n");
+                scanf("%s", &contatti[i].nome);
+                printf("Inserisci il cognome del nuovo contatto:\n");
+                scanf("%s", &contatti[i].cognome);
+                printf("Inserisci il numero di telefono del nuovo contatto:\n");
+                scanf("%s", &contatti[i].telefono);
+                i++;
+                break;
+            case 2:
+                for (int j = 0; j < i; j++)
+                {
+                    printf("Nome:\t\t%s\nCognome:\t%s\nTelefono:\t%s\n\n", contatti[j].nome, contatti[j].cognome, contatti[j].telefono);
+                }
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                controllo = 1;
+                break;
+            default:
+                break;
+        }
+
+    }
+
+
 
     return 0;
 }
