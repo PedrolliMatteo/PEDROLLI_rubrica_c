a7f50b1dc7311d47db0281b43a9c3317a801522b
PedrolliMatteo <Pedro@HP.com>
2025-09-27 16:46:14 +0200

    First commit

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
+//ricorda di mettere prima le struct e poi il main
+
+struct Contatto
+{
+    char nome[30];
+    char cognome[30];
+    char telefono[15];
+};
+
 int main(void) {
-    printf("Hello, World!\n");
+
+    struct Contatto contatti[20];
+
     return 0;
 }
